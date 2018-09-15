dd38850ebf78dc6fdf5cd4c19135e0b5c4ba3aa3
Fadlurss <fadlurss@gmail.com>
2018-09-15 17:01:43 +0700

    error sidenav

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,9 +1,6 @@
 var express    = require('express');
     router = express.Router();
 
-    router.all('/*', (req, res, next)=>{
-        req.app.locals.layout = 'admin';
-    });
 
     router.get('/', (req,res)=>{
         res.render('v_admin/index');
