58acfdc675e7863b4c0e2e5e6f599efcf2ac3896
Fadlurss <fadlurss@gmail.com>
2018-09-17 19:44:42 +0700

    problem in section 18 lecture 148

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(flash());
 app.use((req,res,next)=>{
     res.locals.success = req.flash('success');
     app.locals.moment = require('moment');
+    res.locals.error = req.flash('error');
     next();
 });
 app.use('/', home);
--- routes/home/index.js
+++ routes/home/index.js
@@ -1,7 +1,9 @@
 var express    = require('express');
     router     = express.Router();
+    bcrypt     = require('bcryptjs');
     Posts      = require('../../models/posts');
     Categories = require('../../models/categories');
+    Users      = require('../../models/users');
 
 
     router.get("/", (req,res)=>{
@@ -23,6 +25,80 @@ router.get("/login", ((req,res)=>{
 router.get("/register", ((req,res)=>{
     res.render("v_home/register");
 }));
+
+router.post('/register', (req,res)=>{ 
+
+    var error   = [];
+
+
+        if(!req.body.firstName){
+            error.push({message: 'please add a firstName'});
+        }
+
+        if(!req.body.lastName){
+            error.push({message: 'please add a lastName'});
+        }
+
+        if(!req.body.email){
+            error.push({message: 'please add a email'});
+        }
+
+        if(!req.body.password){
+            error.push({message: 'please add a password'});
+        }
+
+        if(!req.body.passwordConfirm){
+            error.push({message: 'please add a confirm password'});
+        }
+
+        if(req.body.password !== req.body.passwordConfirm){
+            error.push({message: "Password field don't match"});
+        }
+
+        if(error.length > 0 ){
+            res.render('v_home/register', {
+                error: error,
+                firstName : req.body.firstName,
+                lastName  : req.body.lastName,
+                email     : req.body.email,
+            });
+        } 
+        
+        else {
+
+            Users.findOne({email: req.email}).then(user=>{
+                if(!user){
+                    var newUsers = new Users({
+                        firstName : req.body.firstName,
+                        lastName  : req.body.lastName,
+                        email     : req.body.email,
+                        password  : req.body.password
+        
+                    });
+        
+                    bcrypt.genSalt(10, (err, salt)=>{
+                        bcrypt.hash(newUsers.password, salt, (err,hash)=>{
+        
+                          newUsers.password = hash;
+        
+                            newUsers.save();
+                            req.flash('success', 'You are now registered, please login');
+                            res.redirect('/login'); 
+        
+                        });
+                    });
+
+                } else {
+                    req.flash('error', 'That email exist please login');
+                    res.redirect('/login');
+                }
+            });
+
+            
+
+           
+        }
+}); 
 
 router.get("/posts/:id", (req,res)=>{
     Posts.findById(req.params.id, (err, posts)=>{
